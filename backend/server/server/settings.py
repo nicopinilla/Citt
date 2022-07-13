560c4ce68a907092e13291d63568b81e0a79b95c
nicopinilla <selaynn@outlook.com>
2022-07-13 14:00:22 -0400

    setup django project

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^lw0)9982b8nt^^-t34967jnl(n7iojl!q+!5xojw9mr6u0h9f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','127.0.0.1:8000']
 
 
 # Application definition
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.views import debug
 
 from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', debug.default_urlconf),
 ]
 
 urlpatterns += endpoints_urlpatterns
